516a3a4a4dccaad983dfc7998e17e1c87a200604
eddy lee <eddy@gfxlabs.io>
2022-04-18 10:19:51 +0000

    aaa
    Former-commit-id: 8f159a876bfa886e23bede9d1fd309bcc3b2d0ea [formerly 59b136bdc81e8def1315d916ef6316264e282310]
    Former-commit-id: 8c9f2de96da1de63b2308f9c918c6f3f89382c49

--- src/components/util/fadeContainer/fadeContainer.tsx
+++ src/components/util/fadeContainer/fadeContainer.tsx
@@ -1,17 +1,14 @@
 import { Box } from "@mui/material";
-import React, { FC, ReactNode } from "react";
-import { Fade } from "react-awesome-reveal";
+import React, { FC, ReactElement, ReactNode } from "react";
+import { Fade, FadeProps } from "react-awesome-reveal";
 
 export interface FadeContainer {
     children: ReactNode;
 }
 
-export const FadeContainer: FC<FadeContainer> = (props) => {
+export const FadeContainer: any = (props: FadeContainer) => {
     const { children } = props;
-
-    return (
-        <Fade direction="up" triggerOnce>
-            <Box>{children}</Box>
-        </Fade>
-    );
+    return (<Fade direction="up" triggerOnce>
+        <Box>{children}</Box>
+    </Fade>)
 };
