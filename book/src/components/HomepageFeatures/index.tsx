f3693ef39f952e2c95b5d49d565bd471722e061b
eddy lee <eddy@gfxlabs.io>
2022-04-24 10:19:33 +0000

    change copy

--- book/src/components/HomepageFeatures/index.tsx
+++ book/src/components/HomepageFeatures/index.tsx
@@ -10,32 +10,29 @@ type FeatureItem = {
 
 const FeatureList: FeatureItem[] = [
     {
-        title: 'Concepts',
+        title: 'Learn',
         Svg: require('@site/static/img/logo.svg').default,
         description: (
             <>
-                Docusaurus was designed from the ground up to be easily installed and
-                used to get your website up and running quickly.
+                Learn about Interest Protocol!
             </>
         ),
     },
     {
-        title: 'Guides',
+        title: 'Ask',
         Svg: require('@site/static/img/logo.svg').default,
         description: (
             <>
-                Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-                ahead and move your docs into the <code>docs</code> directory.
+                Questions? Join our discord and ask away!
             </>
         ),
     },
     {
-        title: 'Reference',
+        title: 'Contribute',
         Svg: require('@site/static/img/logo.svg').default,
         description: (
             <>
-                Extend or customize your website layout by reusing React. Docusaurus can
-                be extended while reusing the same header and footer.
+                Make your first contribution
             </>
         ),
     },
