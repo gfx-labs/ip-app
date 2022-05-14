cef7fd80bb676780b7f0964f108022c51fd4013e
Henry Han <hanhenry88@gmail.com>
2022-05-14 13:32:45 -0500

    conenct wallet, test chain

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,27 @@
 import { formatColor, formatGradient, gradient, neutral } from "../theme";
 import { Box, Grid, Typography, useMediaQuery, useTheme } from "@mui/material";
 import { useWeb3Context } from "../components/libs/web3-data-provider/Web3Provider";
-import { BaseSwitch } from "../components/util/switch";
 import { ProtocolStatsCard } from "../components/util/cards";
 import { useLight } from "../hooks/useLight";
 import { UsdiGraphCard } from "../components/util/cards/UsdiGraphCard";
 import { StatsMeter } from "../components/util/statsMeter";
 import { UserStats } from "../components/util/UserStats";
+import { ConnectWalletButton } from "../components/util/button";
 
 const LandingPage = () => {
   const theme = useTheme();
-  const { currentAccount } = useWeb3Context();
+  const { currentAccount, connected } = useWeb3Context();
 
   const isMobile = useMediaQuery(theme.breakpoints.down("md"));
 
   const isLight = useLight();
-
 
   return (
     <Box
       sx={{
         marginX: "auto",
         position: "relative",
-        overflowY: "hidden",
+        overflow: "hidden",
       }}
     >
       <Box
@@ -35,14 +34,18 @@ const LandingPage = () => {
         position="relative"
         sx={{
           [theme.breakpoints.down("md")]: {
-            mb: 16,
-
+            mb: 0,
             pb: 0,
             marginLeft: "auto",
           },
         }}
       >
-        <Typography variant="body1" paddingLeft={{xs: 2, md: 6}} marginBottom={2} color={formatColor(neutral.gray10)}>
+        <Typography
+          variant="body1"
+          paddingLeft={{ xs: 2, md: 6 }}
+          marginBottom={2}
+          color={formatColor(neutral.gray10)}
+        >
           Protocol Stats
         </Typography>
         <Box
@@ -59,11 +62,37 @@ const LandingPage = () => {
           <ProtocolStatsCard />
           <UsdiGraphCard />
         </Box>
-        <Box sx={{ marginY: 4 }}>
-          <StatsMeter />
-        </Box>
 
-        <UserStats />
+        <Box sx={{ position: "relative" }}>
+          {!connected && (
+            <Box
+              sx={{
+                position: "absolute",
+                zIndex: 9,
+                top: '-2%',
+                bottom: -16,
+                left: "-100%",
+                right: "-100%",
+                width: "auto",
+                height: "102%",
+                background: isLight
+                  ? "rgba(55, 66, 82, 0.42)"
+                  : "rgba(35, 40, 48, 0.82)",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <ConnectWalletButton invertLight />
+            </Box>
+          )}
+
+          <Box sx={{ marginY: 4 }}>
+            <StatsMeter />
+          </Box>
+
+          <UserStats />
+        </Box>
       </Box>
     </Box>
   );
