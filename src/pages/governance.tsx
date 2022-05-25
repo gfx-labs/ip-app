a67a35fe8bb5ef440295076856d95046b0d24146
Henry Han <hanhenry88@gmail.com>
2022-05-25 18:42:16 -0500

    Governance
    Former-commit-id: 44198e5ba3ae9c6c6d8da82bff9cb917fc4748c6 [formerly 5f998e8756d4fd2860afc33ec06790d03d7f8cac]
    Former-commit-id: 9cd7a9698139aaf60df8416511e56dd8286f3f57

--- src/pages/governance.tsx
+++ src/pages/governance.tsx
@@ -1,8 +1,43 @@
-import React from 'react'
+import { Box, useTheme } from "@mui/material";
+import { useState } from "react";
+import {
+  Proposal,
+  ProposalCard,
+} from "../components/util/governance/ProposalCard";
 
 export const Governance = () => {
-  
+  const theme = useTheme();
+  const [proposals, setProposals] = useState<Proposal[]>([
+    {
+      votes: "11",
+      title: "Emergency vote for 323811",
+      yesVotes: "5",
+      noVotes: "9",
+      status: "active",
+      timeLeft: "30 min",
+    },
+  ]);
   return (
-    <div>Governance</div>
-  )
-}
+    <Box
+      maxWidth="xl"
+      py={{ xs: 7, sm: 0 }}
+      px={{ xs: 2, md: 10 }}
+      minHeight="80vh"
+      margin="auto"
+      position="relative"
+      sx={{
+        [theme.breakpoints.down("md")]: {
+          mb: 0,
+          pb: 0,
+          marginLeft: "auto",
+        },
+      }}
+    >
+      {proposals.map((proposal, index) => (
+        <Box key={index}>
+          <ProposalCard proposal={proposal} />
+        </Box>
+      ))}
+    </Box>
+  );
+};
