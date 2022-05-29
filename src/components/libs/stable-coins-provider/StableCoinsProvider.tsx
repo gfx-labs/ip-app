a2a7b30bd920b4aca191ffa582591464dfcd09ef
Henry Han <Hanhenry88@gmail.com>
2022-05-28 20:26:24 -0500

    initial usdc and usdi balance load faster
    Former-commit-id: 387383ed3cb5a29e2b52e28e7f1325d6fa05ea2b [formerly cfeeecbd775e65dbffa1177da522f36b00ef2ebe]
    Former-commit-id: 30dc6ae79296d0b4dd77647088879df78e2c06db

--- src/components/libs/stable-coins-provider/StableCoinsProvider.tsx
+++ src/components/libs/stable-coins-provider/StableCoinsProvider.tsx
@@ -1,10 +1,6 @@
 import { ethers } from "ethers";
-import React, {
-  useContext,
-  useEffect,
-  useState,
-} from "react";
-import { getStablecoins,  Token } from "../../../chain/tokens";
+import React, { useContext, useEffect, useState } from "react";
+import { getStablecoins, Token } from "../../../chain/tokens";
 import { useBalanceOf } from "../../../hooks/useBalanceOf";
 import { useRolodexContext } from "../rolodex-data-provider/RolodexDataProvider";
 import { useWeb3Context } from "../web3-data-provider/Web3Provider";
@@ -23,36 +19,33 @@ export const StableCoinsProvider = ({
 }: {
   children: React.ReactElement;
 }) => {
-  const { currentAccount, dataBlock, chainId} = useWeb3Context();
+  const { currentAccount, dataBlock, chainId } = useWeb3Context();
   const rolodex = useRolodexContext();
 
-  const [USDC, setUSDC] = useState<Token>(
-    () => getStablecoins(rolodex!).USDC!
-  );
-  const [USDI, setUSDI] = useState<Token>(
-    () => getStablecoins(rolodex!).USDI!
-  );
+  const [USDC, setUSDC] = useState<Token>(() => getStablecoins(rolodex!).USDC!);
+  const [USDI, setUSDI] = useState<Token>(() => getStablecoins(rolodex!).USDI!);
 
   useEffect(() => {
     if (rolodex && rolodex?.addressUSDC) {
-      useBalanceOf(
-        currentAccount,
-        rolodex.addressUSDC,
-        rolodex.provider,
-      ).then((res) => {
-        setUSDC({ ...USDC, wallet_balance: res, wallet_amount: res })});
+      useBalanceOf(currentAccount, rolodex.addressUSDC, rolodex.provider).then(
+        (res) => {
+          setUSDC({ ...USDC, wallet_balance: res, wallet_amount: res });
+        }
+      );
     }
-  }, [currentAccount, dataBlock, chainId]);
+  }, [currentAccount, dataBlock, chainId, rolodex]);
 
   useEffect(() => {
     if (rolodex && rolodex?.addressUSDI) {
-       useBalanceOf(
+      useBalanceOf(
         currentAccount,
         rolodex.addressUSDI,
-        rolodex.provider,
-      ).then((res) => setUSDI({ ...USDI, wallet_balance: res, wallet_amount: res }));
+        rolodex.provider
+      ).then((res) =>
+        setUSDI({ ...USDI, wallet_balance: res, wallet_amount: res })
+      );
     }
-  }, [currentAccount, dataBlock, chainId]);
+  }, [currentAccount, dataBlock, chainId, rolodex]);
 
   return (
     <StableCoinsContext.Provider value={{ USDC, USDI }}>
