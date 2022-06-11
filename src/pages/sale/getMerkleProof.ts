44b5019b3f3c5790b30e4b2af8b62404eb34a780
a <a@a.a>
2022-06-10 19:46:28 -0500

    update merkle proof

--- src/pages/sale/getMerkleProof.ts
+++ src/pages/sale/getMerkleProof.ts
@@ -5,27 +5,15 @@ import { wave1 } from "../whitelist/wave1";
 import { wave2 } from "../whitelist/wave2";
 import {BigNumberish} from "ethers";
 
-//ropsten addresses
 const keyAmounts:BigNumberish[]= [0,BN("1000000e6"),BN("500000e6"),0]
-let root1: string;
-let merkleTree1: MerkleTree;
 const initMerkle = async (wave: number) => {
-  const whitelist1 = wave === 1 ? wave1 : wave2;
-  let leafNodes = [
-    "0xad8B917596d9e6A970393F089dCff0A9c9858934",
-    "0xA8F5d96E2DDfb5ec3F24B960A5a44EbC620064A3",
-    "0x3Df70ccb5B5AA9c300100D98258fE7F39f5F9908",
-    "0x50818e936aB61377A18bCAEc0f1C32cA27E38923",
-    "0xbA99c822bb4dd80f046a75EE564f8295D44Da743",
-    "0x8bAf8b6Ed0E0ddB6557Af1A7391a86949FAFa3a8",
-    "0x0E1456214D8b4FEc597639a475C49c6682D94B09",
-    "0x2243b90CCaF4a03F7289502722D8665E3d4f2972"
-].map((addr) =>
+  const whitelist = wave === 1 ? wave1 : wave2;
+  let leafNodes = Array.from(whitelist).map((addr) =>
     solidityKeccak256(["address", "uint256"], [addr, keyAmounts[wave]])
   );
-  merkleTree1 = new MerkleTree(leafNodes, keccak256, { sortPairs: true });
-  root1 = merkleTree1.getHexRoot();
-  console.log("root",root1)
+  let merkleTree = new MerkleTree(leafNodes, keccak256, { sortPairs: true });
+  let root = merkleTree.getHexRoot();
+  return {tree: merkleTree, root: root}
 };
 
 export const getSaleMerkleProof = async (
@@ -33,13 +21,12 @@ export const getSaleMerkleProof = async (
   wave: number
 ) :Promise<{proof:string[], key:BigNumberish}>=>{
   try {
-    await initMerkle(wave);
-
+    const {tree, root} = await initMerkle(wave);
     let leaf = solidityKeccak256(
       ["address", "uint256"],
       [currentAccount, keyAmounts[wave]]
     );
-    let proof = merkleTree1.getHexProof(leaf);
+    let proof = tree.getHexProof(leaf);
 
     return {proof, key:keyAmounts[wave]};
   } catch (err) {
