fe753ac19cd9339e1b30a4590bf1c5282f979262
Henry Han <hanhenry88@gmail.com>
2022-09-06 18:09:57 -0500

    Update proposal time

---
+++ src/components/util/governance/proposalTimeRemaining.ts
@@ -0,0 +1,54 @@
+import { IProposalType } from './ProposalCard'
+
+const proposalTimes = {
+  Standard: {
+    pending: 13140,
+    voting: 40320,
+  },
+  Emergency: {
+    pending: null,
+    voting: 6570,
+  },
+  Optimistic: {
+    pending: 25600,
+    voting: 40320,
+  },
+} as {
+  [K in Exclude<IProposalType, ''>]: { pending: number | null; voting: number }
+}
+
+export const proposalTimeRemaining = (
+  proposalType: Exclude<IProposalType, ''>,
+  startingBlock: number,
+  endingBlock: number,
+  currentBlock: number
+) => {
+  const proposalPeriods = proposalTimes[proposalType]
+
+  const blockDiff = currentBlock - startingBlock
+  let blocksRemaining
+  const votingPeriod = proposalPeriods.voting + (proposalPeriods.pending || 0)
+  if (proposalPeriods.pending && blockDiff < proposalPeriods.pending) {
+    blocksRemaining = proposalPeriods.pending - blockDiff
+  } else if (blockDiff < votingPeriod) {
+    blocksRemaining = votingPeriod - blockDiff
+  } else {
+    blocksRemaining = endingBlock - currentBlock
+  }
+
+  return getTimeRemaining(blocksRemaining)
+}
+
+const getTimeRemaining = (blockDiff: number) => {
+  const secs = blockDiff * 13.5
+
+  const hrdiff = Math.abs(Math.round((100 * secs) / (60 * 60)) / 100)
+
+  if (hrdiff >= 24) {
+    return `Time Remaining: ${Math.floor(hrdiff / 24)} days`
+  } else if (hrdiff > 1) {
+    return `Time Remaining: ${Math.floor(hrdiff)} hours`
+  } else {
+    return `Time Remaining: ${Math.floor(hrdiff * 60)} minute(s)`
+  }
+}
--- src/components/util/helpers/getAPIBaseUrl.ts
+++ src/components/util/helpers/getAPIBaseUrl.ts
@@ -1,4 +1,4 @@
-import { DEFAULT_BASE_URL } from '../../../constants'
+import { ANALYTICS_URL } from '../../../constants'
 
 let SET_BASE_URL = ''
 
@@ -9,8 +9,8 @@ const getAPIBaseUrl = () => {
 
   const envURL: string | undefined = import.meta.env.VITE_ANALYTICS_URL
   if (envURL === '' || envURL === undefined) {
-    SET_BASE_URL = DEFAULT_BASE_URL
-    return DEFAULT_BASE_URL
+    SET_BASE_URL = ANALYTICS_URL
+    return ANALYTICS_URL
   }
   SET_BASE_URL = envURL
   return envURL
