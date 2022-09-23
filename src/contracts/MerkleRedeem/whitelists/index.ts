70fb70055441a551b3bbab97dc48a9f33ae9423f
Henry Han <hanhenry88@gmail.com>
2022-09-23 17:09:56 -0500

    week 13

--- src/contracts/MerkleRedeem/whitelists/index.ts
+++ src/contracts/MerkleRedeem/whitelists/index.ts
@@ -4,6 +4,7 @@ import week9 from '../../../pages/whitelist/week9'
 import week10 from '../../../pages/whitelist/week10'
 import week11 from '../../../pages/whitelist/week11'
 import week12 from '../../../pages/whitelist/week12'
+import week13 from '../../../pages/whitelist/week13'
 
 const weeks = {
   7: week7,
@@ -12,6 +13,7 @@ const weeks = {
   10: week10,
   11: week11,
   12: week12,
+  13: week13,
 }
 
 export default weeks
