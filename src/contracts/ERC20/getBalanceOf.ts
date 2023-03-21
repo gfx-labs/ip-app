937a73c1af67262a83a23f5fd6026f210eb9cd63
a <eddy@gfxlabs.io>
2023-03-21 17:00:00 +0000

    Devel

--- src/contracts/ERC20/getBalanceOf.ts
+++ src/contracts/ERC20/getBalanceOf.ts
@@ -3,12 +3,20 @@ import getDecimals from '../misc/getDecimals'
 import { useFormatBNtoPreciseStringAndNumber } from '../../hooks/useFormatBNWithDecimals'
 import { ERC20Detailed__factory } from '../../chain/contracts'
 import { BigNumber } from 'ethers'
+import { BN } from '../../easy/bn'
 
 export const getBalanceOf = async (
   wallet_address: string,
   contract_address: string,
   providerOrSigner: JsonRpcProvider | JsonRpcSigner
 ): Promise<{ num: number; str: string; bn: BigNumber }> => {
+  if(wallet_address == undefined || wallet_address == "" ) {
+    return {
+      num: 0,
+      str: "0",
+      bn: BN(0),
+    }
+  }
   const contract = ERC20Detailed__factory.connect(
     contract_address,
     providerOrSigner
--- src/contracts/MerkleRedeem/getClaimStatus.ts
+++ src/contracts/MerkleRedeem/getClaimStatus.ts
@@ -1,4 +1,5 @@
 import { JsonRpcProvider, JsonRpcSigner } from '@ethersproject/providers'
+import { log, trace } from '../../easy/log'
 import { Contract } from 'ethers'
 import { MerkleRedeem__factory } from '../../chain/contracts/factories/IPTsale/MerkleRedeem'
 import { MERKLE_REDEEM_ADDRESS } from '../../constants'
@@ -26,19 +27,29 @@ const getLatestWeek = async (merkleContract: Contract) => {
 const getClaimStatusOf = async (
   account: string,
   providerOrSigner: JsonRpcProvider | JsonRpcSigner
-) => {
+):Promise<number[]> => {
   try {
     const merkleContract = MerkleRedeem__factory.connect(
       MERKLE_REDEEM_ADDRESS,
       providerOrSigner
     )
 
+    log.trace("attempting to get claim status", account)
     const latestWeek = await getLatestWeek(merkleContract)
     const claimStatus = await merkleContract.claimStatus(account, 7, latestWeek)
+    let idx = 7
+    const weeks = []
+    for(let i = 0; i < claimStatus.length; i++) {
+      if(claimStatus[i] === false) {
+        weeks.push(idx)
+      }
+      idx = idx + 1
+    }
+    log.trace("claim status", account, claimStatus, weeks)
 
-    return claimStatus
+    return weeks
   } catch (err) {
-    console.error(err)
+    log.warn("getting status", err)
     throw new Error('Error getting claim status')
   }
 }
